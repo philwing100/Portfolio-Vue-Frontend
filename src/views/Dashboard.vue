9caee1783aeb769c29ff764931527ebcb19c7aa3
philwing100 <philwing100@gmail.com>
2024-02-05 23:01:11 -0600

    Made basic dashboard skeleton

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,21 +1,65 @@
 <template>
-  <div class="dashboard">
-    <h1 class="gey"></h1>
+  <div class="grid-container">
+    <div class="heroText">Current Task: Work on Dashboard</div>
+    <div class="bigButton previousDay">Previous Day</div>
+    <div class="bigButton nextDay">Next Day</div>
+    <div class="listContainer allTasks">All tasks</div>
+    <div class="listContainer todayTasks">Today's Tasks</div>
+
+    <div class="calendarTemp">Insert Daily Calendar Here</div>
+
   </div>
+
 </template>
 
 <script>
 
 export default {
+//import colors from '@/assets/colors.json';
+
+
   name: 'DashboardWorld',
   components: {
-    
   }
 }
 </script>
 
 <style>
-.gey{
-  font-size:300px;
+.grid-container{
+  display:grid;
+  grid-template-columns: 250px 250px 250px 250px 250px 250px;
+  grid-template-rows: 100px ifr;
+  height:100vh;
+}
+
+.heroText{
+  font-size:30px;
+}
+
+.bigButton{
+
+}
+
+.previousDay{
+ grid-column:3;
+}
+
+.nextDay{
+  grid-column:4;
+}
+
+.allTasks{
+  grid-column:1;
+  grid-row:2;
+}
+
+.todayTasks{
+  grid-column:2;
+  grid-row:2;
+}
+
+.calendarTemp{
+  grid-column:3;
+  grid-row:2;
 }
 </style>
