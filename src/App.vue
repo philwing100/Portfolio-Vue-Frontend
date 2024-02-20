6198e0d1cbb77411d2c0841b9cbba7f09af2ed77
philwing100 <philwing100@gmail.com>
2024-02-19 22:30:51 -0600

    Reorganized files to prepare for making lists components

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import SideBar from './components/SideBar.vue';
+import SideBar from './components/SidebarComponents/SideBar.vue';
 
 export default {
   components: {
---
+++ src/components/ListItems/PopupContainer.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="popup-overlay">
+      <div class="popup">
+        <span class="close" @click="$emit('close')">X</span>
+        <div class="popup-content">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'PopupContainer',
+    setup() {
+
+      return {
+      };
+    },
+
+    }
+</script>
+
+<style>
+.popup-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  position: relative;
+}
+
+.close {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  cursor: pointer;
+}
+
+.popup-content {
+  /* Add additional styles for popup content */
+}
+
+
+</style>
