11a2f4a8432fbbd64bd2a4c6a70bd73f555f5197
philwing100 <philwing100@gmail.com>
2024-10-29 12:33:41 -0500

    Begin adding google login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="bruh">
-  <LoginComponent/>
+    <LoginComponent />
   </div>
+  <h1>Sign in</h1>
+  <a class="button google" href="/login/federated/google">Sign in with Google</a>
 </template>
 
 <script>
 import LoginComponent from '@/components/Login.vue';
+//Needs to also display the sign up with google
+
 
 export default {
   name: 'LoginWorld',
-  components:{
+  components: {
     LoginComponent
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('http://localhost:3000/api/signup', {
+          email: this.email,
+          password: this.password
+        });
+        this.message = 'Signup successful! Please log in.';
+        // Optionally, you can redirect the user to the login page
+        setTimeout(() => {
+          this.$router.push('/login');
+        }, 2000); // Redirect after 2 seconds
+      } catch (error) {
+        console.log(error.response.data.errors);
+        this.message = 'Error signing up: ' + (error.response.data.errors || error.message);
+      }
+    },
   }
 }
 </script>
 
-
-<style>
-</style>
+<style></style>
